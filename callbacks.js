d93598c76f4cf1d4b8d536c49ba336b482f75109
Dan Smestad <hockeydan25@gmail.com>
2021-10-13 10:37:16 -0500

    first push,Lab 7 video 1/7 files: delay,what time is it, button,callbacks.

---
+++ callbacks.js
@@ -0,0 +1,7 @@
+let animals = ['Giraffe','Elephant','Yak']//array
+
+/*function animals up to 3 arguments froeach calls back 3 times forEach element in the array aniamls.
+A specified action will be prefomed that we specify with the animals.forEach function.*/
+animals.forEach(function(animal, index){//argument added index.
+    //console.log(animal, index) //shows what is in the array and what place in the array as index.
+} )
